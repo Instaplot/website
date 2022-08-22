741af850b42520858fbc7bf7ae198eaa1ab00193
VaibhavSys <vaibhavsys@protonmail.com>
2022-08-22 23:19:26 +0530

    chore: remove burger stuff from header

--- components/HeaderPage.vue
+++ components/HeaderPage.vue
@@ -1,16 +1,6 @@
 <template>
   <el-header>
-    <el-row>
-      <el-col :span="24">
-        <el-breadcrumb separator="/">
-          <el-breadcrumb-item>
-            <router-link to="/">Home</router-link>
-          </el-breadcrumb-item>
-          <el-breadcrumb-item>
-            <router-link to="/contact">Contact</router-link>
-          </el-breadcrumb-item>
-        </el-breadcrumb>
-      </el-col>
-    </el-row>
+    <router-link to="/">Home</router-link>
+    <router-link to="/contact">Contact</router-link>
   </el-header>
 </template>
